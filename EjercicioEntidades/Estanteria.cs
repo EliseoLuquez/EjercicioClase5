dcd3a134c5c57dd19c38748323178922e063d7a0
alumno <alumno@utnfra.com.ar>
2019-04-01 22:08:49 -0300

    first commit

---
+++ EjercicioEntidades/Estanteria.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEntidades
+{
+    public class Estanteria
+    {
+        private int estanteriaUbicacion;
+        private Producto[] productos;
+
+
+        private Estanteria(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estanteria(int capacidad, int ubicacion): this(capacidad)
+        {   
+            this.estanteriaUbicacion = ubicacion;
+        }
+
+        public Producto[] GetProducto()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estanteria e)
+        {
+            string salida = e.estanteriaUbicacion.ToString();
+
+            foreach (Producto producto in e.productos)
+            {
+                if(!(producto is null))
+                    salida += (" " +  Producto.MostrarProducto(producto));
+            }
+            return salida;
+        }
+
+        public  static  bool operator ==(Estanteria e, Producto p)
+        {
+            foreach (Producto producto in e.productos)
+            {
+                if (producto == p)
+                    return true;
+            }
+            return false;
+        }
+
+
+        public static bool operator !=(Estanteria e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estanteria e, Producto p)
+        {
+            if (e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if(e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }                
+            }
+
+            return false;
+        }
+
+        public static Estanteria operator -(Estanteria e, Producto p)
+        {
+            
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                        i = e.productos.Length;
+                    }
+                }            
+            return e;
+        }
+    }
+}
---
+++ EjercicioEntidades/Producto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEntidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            this.marca = marca;
+            this.precio = precio;
+            this.codigoDeBarra = codigoDeBarra;
+        }
+
+        public string GetMarca()
+        {
+
+            return this.marca;
+
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+
+        }
+
+        public static string MostrarProducto(Producto p)
+        {   
+            return  "Codigo de barra :"  + (string)p +  "  Marca:" + p.GetMarca() + " Precio:" + p.GetPrecio();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Producto)
+                return this == (Producto)obj;
+            else if (obj is string )
+                return this == (string)obj;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto a, Producto b)
+        {
+            if ((a is null) || (b is null))
+                   return false;
+            return ((string)a == (string)b && a.GetMarca() == b.GetMarca());
+                
+        }
+
+        public static bool operator !=(Producto a, Producto b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator ==(Producto a, string marca)
+        {
+            return a.GetMarca() == marca;// ? true : false;
+        }
+
+        public static bool operator !=(Producto a, string marca)
+        {
+            return !(a == marca);
+        }
+
+
+    }
+}
